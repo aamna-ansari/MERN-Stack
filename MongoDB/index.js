468e39c319fe2869ff7a0916581fd1cdb230eda0
aamna-ansari <aamnansari29@gmail.com>
2025-01-31 21:55:44 +0500

    create schema

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,16 +1,33 @@
-const express = require('express');
-const users = require("./MOCK_DATA.json")
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 8000;
 
+// Create schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email:{
+    type:String,
+    require:true,
+    unique: true
+  }
+});
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+
 // Create routes
+app.get("/users", (req, res) => {
+  res.send(`<h1>Hi, I am here from Get Method</h1>`);
+});
 
-app.get('/users', (req,res)=>{
-    res.send(`<h1>Hi, I am here from Get Method</h1>`)
-})
-
-
-// Run Server 
-app.listen(PORT, ()=>{
-    console.log(`Server run at port http://localhost:${PORT}`)
-})
+// Run Server
+app.listen(PORT, () => {
+  console.log(`Server run at port http://localhost:${PORT}`);
+});
