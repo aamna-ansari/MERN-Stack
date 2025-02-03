12360c34316e9251c2cbc39b1e64b1a93e775ba0
aamna-ansari <aamnansari29@gmail.com>
2025-02-03 19:53:10 +0500

    router

--- Shortener-url/routes/url.js
+++ Shortener-url/routes/url.js
@@ -1,8 +1,16 @@
+// Step 1:
 const express = require('express');
 
-// Router
+// step4: Controller Function  destructure
+const {generatedNewShortUrl} = require('../controllers/url');
+
+// Step2:Router
 const router = express.Router;
 
-// Create main route POST for generte random-id
 
-router.post('/');
+//Step3: Create main route POST for generte random-id
+
+router.post('/', generatedNewShortUrl );
+
+// export 
+module.exports = router;
