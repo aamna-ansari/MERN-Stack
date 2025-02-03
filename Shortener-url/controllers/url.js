f9c445e5bc3ca9964377c97fac8558b854357526
aamna-ansari <aamnansari29@gmail.com>
2025-02-03 19:50:07 +0500

    generatedNewShortUrl created

--- Shortener-url/controllers/url.js
+++ Shortener-url/controllers/url.js
@@ -1,3 +1,22 @@
+const {nanoid}= require('nanoid');
+// Store in data base so , import this 
+const URL = require('../models/url');
+
+
 async function generatedNewShortUrl(req, res){
-    
+    const body = req.body;
+    if(!body.url) return res.status(400).json({error: 'URL is required'})
+    const shortID = nanoid(8); //Generate 8 characters short id by nanoid
+// Add in DataBase  
+    await URL.create({
+        shortId: shortID,
+        redirectUrl: body.url,
+        visitHistory:[], 
+    });
+    return res.json({id: shortID});
+}
+
+// export 
+module.exports = {
+    generatedNewShortUrl,
 }
