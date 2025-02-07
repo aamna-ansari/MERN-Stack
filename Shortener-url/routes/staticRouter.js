dc7451942380fa5f7324c556396c8d25b705198e
aamna-ansari <aamnansari29@gmail.com>
2025-02-07 22:12:15 +0500

    refactor: update URL fetching and form action in home.ejs

--- Shortener-url/routes/staticRouter.js
+++ Shortener-url/routes/staticRouter.js
@@ -3,9 +3,9 @@ const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const allUrls = await URL.find({});
-  console.log(allUrls);
-  return res.render("home", {urls: allUrls});
+  const allurls = await URL.find({});
+  console.log(allurls);
+  return res.render("home", {urls: allurls});
 });
 
 module.exports = router;
